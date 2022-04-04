19d783e2a6c58f8d90e327740cac0c203eb76ef8
21bca74 <96609250+21bca74@users.noreply.github.com>
2022-04-04 11:01:03 +0530

    Add files via upload

---
+++ solution 19.py
@@ -0,0 +1,14 @@
+l=[]
+count=0
+def student():
+    for i in range(5):
+        a=input("Enter student name:")
+        l.append(a)
+        s=input("Enter student address:")
+        l.append(a)
+    return(1)
+a=student()
+print(a)
+s=student()
+print(a)
+55
